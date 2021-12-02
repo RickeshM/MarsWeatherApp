9957aa6519981038447c83fb7092eb33a267caf9
RickeshM <rickesh.mysuria@gmail.com>
2021-12-02 14:27:37 -0800

    styled previous weather section days

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
+// show/hide previous weather section
 const togglePrevWeather = document.querySelector(".previous-weather-hide-arrow");
-
 const prevWeatherSection = document.querySelector(".previous-weather-section");
-
 togglePrevWeather.addEventListener("click", () => {
     prevWeatherSection.classList.toggle("show-weather");
 });
+
+// InSight's weather API Documentation: https://api.nasa.gov/assets/insight/InSight%20Weather%20API%20Documentation.pdf
