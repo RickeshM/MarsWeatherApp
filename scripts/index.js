dd5778b7122fc712e6f35f42b56a3c503b9150a7
RickeshM <rickesh.mysuria@gmail.com>
2022-01-18 17:48:49 -0800

    remove whitespace in code

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@
 const API_KEY = "DEMO_KEY";
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
-// CSS show/hide previous weather section
 const togglePrevWeatherArrow = document.querySelector(".previous-weather-hide-arrow");
 const prevWeatherSection = document.querySelector(".previous-weather-section");
 
